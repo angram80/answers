d73b42934e7e70ca9b2fce147520c93007df5dd2
angram80 <79212298+angram80@users.noreply.github.com>
2021-03-03 11:23:30 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,55 @@
-
+basic.forever(function () {
+    basic.showIcon(IconNames.Heart)
+    music.playMelody("E B C5 A B G A F ", 125)
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . # # # .
+        . . # . .
+        . . . . .
+        `)
+    music.playMelody("G F G A - F E D ", 125)
+    basic.showLeds(`
+        # . . . #
+        . # . # .
+        . . . . .
+        . # . # .
+        # . . . #
+        `)
+    music.playMelody("E B C5 A B G A F ", 125)
+    basic.showLeds(`
+        . # . # .
+        # # # # #
+        # # # # #
+        . # # # .
+        . . # . .
+        `)
+    music.playMelody("G F G A - F E D ", 125)
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . # # # .
+        . . # . .
+        . . . . .
+        `)
+    music.playMelody("E B C5 A B G A F ", 125)
+    basic.showLeds(`
+        # . . . #
+        . # . # .
+        . . . . .
+        . # . # .
+        # . . . #
+        `)
+    music.playMelody("G F G A - F E D ", 125)
+    basic.showLeds(`
+        . # . # .
+        # # # # #
+        # # # # #
+        . # # # .
+        . . # . .
+        `)
+    music.playMelody("E B C5 A B G A F ", 125)
+})
+basic.forever(function () {
+	
+})
